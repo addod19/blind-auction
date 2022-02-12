d684d082f3aa4a85fca13b2ebe66a44fe29e367f
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-02-12 05:17:43 +0000

    Rename contract and declare variables

--- contracts/BlindBid.sol
+++ contracts/BlindBid.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Blindbid {
+contract BlindBid {
   // Varialbles
+
+  struct Bid {
+    bytes32 blindedBid;
+    uint deposit;
+  }
+
+  address payable public beneficiary;
+  uint public biddingEnd;
+  uint public revealEnd;
+  bool public ended;
+
+  mapping(address => Bid[]) public bids;
+
+  address public highestBidder;
+  uint public highestBid;
+
+  mapping(address => uint) public pendingReturns;
 
    // Modifiers
 
@@ -20,7 +37,7 @@ contract Blindbid {
   constructor(uint _biddingTime, uint _revealTime, address payable _beneficiary) {
     beneficiary = _beneficiary;
     biddingEnd = block.timestamp + _biddingTime;
-    revealTime = biddingEnd + _revealTime;
+    revealEnd = biddingEnd + _revealTime;
   }
 
   function bid() {
